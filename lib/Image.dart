d51a002bced8deefa9daca471b47738f1d5118ca
Manvendra03 <manvendrapatidar03@gmail.com>
2023-10-28 09:56:47 +0530

    secound commit

--- lib/Image.dart
+++ lib/Image.dart
@@ -3,14 +3,21 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 
-class Imagee {
-  static List<Imagee> list = [];
+class Imagee extends ChangeNotifier {
+  List<Imagee> list = [];
 
   final String large;
   final String medium;
   final String small;
 
   Imagee(this.large, this.medium, this.small);
+
+  void addToList(List<Imagee> tempList) async {
+    for (var element in tempList) {
+      list.add(element);
+      notifyListeners();
+    }
+  }
 
   factory Imagee.fromJson(Map<String, dynamic> resp) {
     return Imagee(
--- lib/helper.dart
+++ lib/helper.dart
@@ -4,21 +4,26 @@ import 'package:http/http.dart' as http;
 import 'package:wallpaper_app/Image.dart';
 
 class Helper {
-  static final String url =
-      "https://api.pexels.com/v1/curated?page=2&per_page=40";
-  static final String key =
+  static const String url = "https://api.pexels.com/v1/";
+  static const String key =
       "Dkv18GyTSpu99QcIUvqpGAl5j5epc6NEwONuPGPtbWUkev9XVQJNc9jR";
 
-  static void getPage(int page) async {
-    var uri = Uri.parse(url);
+  Future<List<Imagee>> getPage(int page) async {
+    var route = "curated?page=${page}&per_page=10";
+
+    var uri = Uri.parse(url + route);
     var response = await http.get(uri, headers: {'Authorization': key});
     print('Response status: ${response.statusCode}');
     print('Response body: ${response.body}');
 
     final json = jsonDecode(response.body.toString()) as Map<String, dynamic>;
+
+    List<Imagee> tempList = [];
     for (int i = 0; i < json['per_page']; i++) {
       Imagee temp = await Imagee.fromJson(json['photos'][i]);
-      Imagee.list.add(temp);
+      tempList.add(temp);
     }
+
+    return tempList;
   }
 }
